bbbdea997ddc58aafea6da712f96c8ab2c3619ea
VUD0-0 <devurname@gmail.com>
2023-09-04 22:06:06 +0300

    Подключение к MYSQL

---
+++ app.js
@@ -0,0 +1,24 @@
+const mysql = require("mysql2");
+  
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "new_schema",
+  password: "!QAZ2wsxCDE#4rfv"
+});
+// тестирование подключения
+ connection.connect(function(err){
+    if (err) {
+      return console.error("Ошибка: " + err.message);
+    }
+    else{
+      console.log("Подключение к серверу MySQL успешно установлено");
+    }
+ });
+ // закрытие подключения
+ connection.end(function(err) {
+  if (err) {
+    return console.log("Ошибка: " + err.message);
+  }
+  console.log("Подключение закрыто");
+});
